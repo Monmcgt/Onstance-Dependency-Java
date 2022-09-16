e2146c215b52d47d5af39fec6f609161478555a1
Monmcgt <raninmon2015@gmail.com>
2022-09-16 23:44:48 +0700

    Initial Commit

---
+++ src/main/java/me/monmcgt/code/onstance/packet/Packet.java
@@ -0,0 +1,21 @@
+package me.monmcgt.code.onstance.packet;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public abstract class Packet {
+    public String type;
+
+    public abstract String getType();
+
+    private static final Gson gson = new GsonBuilder().create();
+
+    public static String serialize(Packet packet) {
+        packet.type = packet.getType();
+        return gson.toJson(packet);
+    }
+
+    public static Packet deserialize(String json, Class<? extends Packet> packetClass) {
+        return gson.fromJson(json, packetClass);
+    }
+}
